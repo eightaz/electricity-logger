5a396d4028e6694cb9cb96c42a9e8630fb09bce3
Arūnas Jucys <platforms@flexblow.com>
2024-01-03 15:09:29 +0200

    add new config

--- src/config.ts
+++ src/config.ts
@@ -20,6 +20,30 @@ const consoles: DataConsole[] = [
     name: 'Surinkimas',
     ip: 'http://10.0.7.134',
   },
+  {
+    name: 'Linija 1 liejimas CLF 5488',
+    ip: 'http://10.0.7.135',
+  },
+  {
+    name: 'Linija 1 kaitintuvas 5488',
+    ip: 'http://10.0.7.136',
+  },
+  {
+    name: 'Linija 2 liejimas CLF 5489',
+    ip: 'http://10.0.7.137',
+  },
+  {
+    name: 'Linija 2 kaitintuvas 5489',
+    ip: 'http://10.0.7.138',
+  },
+  {
+    name: 'Linija 3 liejimas CLF 5528',
+    ip: 'http://10.0.7.139',
+  },
+  {
+    name: 'Linija 3 kaitintuvas 5528',
+    ip: 'http://10.0.7.140',
+  },
 ];
 
 export default consoles;
