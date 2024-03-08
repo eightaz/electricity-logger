7d4d18d3a9d10fcddab2a16e4cf4f61eb47e8783
Arūnas Jucys <platforms@flexblow.com>
2024-03-08 15:20:50 +0200

    update electricity measurements

--- src/config.ts
+++ src/config.ts
@@ -1,49 +1,61 @@
-export class DataConsole {
-  name: string;
-  ip: string;
+import axios from "axios";
+export interface CurrentPowerResponse {
+  name: string,
+  power: number | null,
 }
 
-const consoles: DataConsole[] = [
-  {
-    name: 'Liejimas Chilleriai',
-    ip: 'http://10.0.7.131',
-  },
-  {
-    name: 'Kompesorine',
-    ip: 'http://10.0.7.132',
-  },
-  {
-    name: 'MAC',
-    ip: 'http://10.0.7.133',
-  },
-  {
-    name: 'Surinkimas',
-    ip: 'http://10.0.7.134',
-  },
-  {
-    name: 'Linija 1 liejimas CLF 5488',
-    ip: 'http://10.0.7.135',
-  },
-  {
-    name: 'Linija 1 kaitintuvas 5488',
-    ip: 'http://10.0.7.136',
-  },
-  {
-    name: 'Linija 2 liejimas CLF 5489',
-    ip: 'http://10.0.7.137',
-  },
-  {
-    name: 'Linija 2 kaitintuvas 5489',
-    ip: 'http://10.0.7.138',
-  },
-  {
-    name: 'Linija 3 liejimas CLF 5528',
-    ip: 'http://10.0.7.139',
-  },
-  {
-    name: 'Linija 3 kaitintuvas 5528',
-    ip: 'http://10.0.7.140',
-  },
-];
+export interface PosTotalResponse {
+  name: string,
+  total: number | null,
+}
+
+export class DataConsole {
+  private name: string;
+  private ip: string;
+
+  constructor(name: string, ip: string) {
+    this.name = name;
+    this.ip = ip;
+  }
+
+  public async currentPower(): Promise<CurrentPowerResponse> {
+    return axios
+      .get(this.ip + "/api/v1/measurements/p", { decompress: false })
+      .then((r) => ({
+        name: this.name,
+        power: r?.data?.value
+      }))
+      .catch(e => ({
+        name: this.name,
+        power: null
+      }));
+  }
+
+  public async posTotal(): Promise<PosTotalResponse> {
+    return axios
+      .get(this.ip + "/api/v1/meters/ea-pos-total", { decompress: false })
+      .then(r => ({
+        name: this.name,
+        total: r?.data?.value
+      }))
+      .catch(e => ({
+        name: this.name,
+        total: null
+      }))
+  }
+}
+
+const consoles = [
+  new DataConsole('Liejimas Chilleriai', 'http://10.0.7.131'),
+  new DataConsole('Kompesorine', 'http://10.0.7.132'),
+  new DataConsole('MAC', 'http://10.0.7.133'),
+  new DataConsole('Surinkimas', 'http://10.0.7.134'),
+  new DataConsole('Linija 1 liejimas CLF 5488', 'http://10.0.7.135'),
+  new DataConsole('Linija 1 kaitintuvas 5488', 'http://10.0.7.136'),
+  new DataConsole('Linija 2 liejimas CLF 5489', 'http://10.0.7.137'),
+  new DataConsole('Linija 2 kaitintuvas 5489', 'http://10.0.7.138'),
+  new DataConsole('Linija 3 liejimas CLF 5528', 'http://10.0.7.139'),
+  new DataConsole('Linija 3 kaitintuvas 5528', 'http://10.0.7.140'),
+]
 
 export default consoles;
--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,26 @@
 require('dotenv').config();
 import 'reflect-metadata';
-import dataConsoles, { DataConsole } from './config';
+import dataConsoles from './config';
 import { TotalElectricity } from './entity/TotalElectricity';
 import { MomentPower } from './entity/MomentPower';
 import db from './database';
-import axios from 'axios';
 
-async function fetchCurrentPower(console: DataConsole) {
-  return axios
-    .get(console.ip + "/api/v1/measurements/p")
-    .then(r => ({
-      name: console.name,
-      power: r?.data?.value
-    }))
-    .catch(e => ({
-      name: console.name,
-      power: null
-    }));
-}
-
-async function fetchPosTotal(console: DataConsole) {
-  return axios
-    .get(console.ip + "/api/v1/meters/ea-pos-total")
-    .then(r => ({
-      name: console.name,
-      total: r?.data?.value
-    }))
-    .catch(e => ({
-      name: console.name,
-      total: null
-    }))
-}
-
-async function savePosTotal() {
-  const totals = await Promise.all(dataConsoles.map(fetchPosTotal));
+const savePosTotal = async () => {
+  const totals = await Promise.all(dataConsoles.map(console => console.posTotal()));
   const now = new Date();
 
-  const entities = totals.map((total) =>
-    db.getRepository(TotalElectricity).create({
-      time: now,
-      name: total.name,
-      total: total.total,
-    })
-  );
+  const entities = totals.map((total) => db.getRepository(TotalElectricity).create({
+    time: now,
+    name: total.name,
+    total: total.total,
+  }));
 
   await db.manager.save(entities);
   console.log('Saved total power consumption', now);
 }
 
-async function saveCurrentPower() {
-  const powers = await Promise.all(dataConsoles.map(fetchCurrentPower));
+const saveCurrentPower = async () => {
+  const powers = await Promise.all(dataConsoles.map(console => console.currentPower()));
   const now = new Date();
 
   const entities = powers.map((power) =>
